1a41138c9ee989096187e6bb2747c0050d882016
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2021-05-26 16:50:51 -0300

    add nav component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,24 +1,32 @@
 import { NgModule } from '@angular/core';
+// Components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/template/header/header.component';
+import { NavComponent } from './components/template/nav/nav.component'
+import { FooterComponent } from './components/template/footer/footer.component';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// Material Imports
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { FooterComponent } from './components/template/footer/footer.component'
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
