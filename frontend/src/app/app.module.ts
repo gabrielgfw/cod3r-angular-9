570654e8b1bb0d22b49cd089caa93a6ceb489e70
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2021-06-01 11:22:19 -0300

    remove product read2 component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -22,7 +22,6 @@ import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ProductReadComponent } from './components/product/product-read/product-read.component';
-import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
@@ -42,7 +41,6 @@ registerLocaleData(localePt);
     RedDirective,
     ProductCreateComponent,
     ProductReadComponent,
-    ProductRead2Component
   ],
   imports: [
     BrowserModule,
